be55c75d4573f7383a820e11c090c06801fb1b76
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-12 01:05:51 +0600

    add experts component and uiexperts show

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h3>This is Services: {services.length}</h3>
+            <h1 className='headline'>Our Services</h1>
             <div className="services-container">
                 {
                     services.map(service => <Service
