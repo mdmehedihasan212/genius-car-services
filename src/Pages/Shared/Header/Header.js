a49d85164190e9d86706d694ea188aa9be87f7ca
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-12 01:45:16 +0600

    add navbar from react bootstarp and set logo from local folder

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
+import logo from '../../../images/logo-black.png';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is Navbar</h1>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={logo}
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
