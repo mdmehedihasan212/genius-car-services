bec94e2fee12c42ba0c2bbc21dfcf7902be2eb70
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-11 21:01:36 +0600

    add display grid and some design

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Service.css';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, img, description, price } = service;
     return (
-        <div>
-            <h1>Service</h1>
+        <div className='service-container'>
+            <img src={img} alt="img" />
+            <div className="service-details">
+                <h4>{name}</h4>
+                <h5>Price: {price}</h5>
+                <p>{description}</p>
+                <button>{name}</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -10,7 +12,15 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h1>This is Services: {services.length}</h1>
+            <h3>This is Services: {services.length}</h3>
+            <div className="services-container">
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
