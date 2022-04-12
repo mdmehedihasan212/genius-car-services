685cd49f4a8296d68ddf6f160632e0af3d46eb42
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-13 02:13:42 +0600

    add onsubmit function and read value

--- src/Pages/LogIn/Register/Register.js
+++ src/Pages/LogIn/Register/Register.js
@@ -3,16 +3,26 @@ import { Link } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(name, email, password);
+    }
+
     return (
         <div className='form-container'>
             <h1 className='form-title'>Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="text">Name</label>
-                <input type="text" name="Your name" id="" placeholder='Your Name' />
+                <input type="text" name="name" id="1" placeholder='Your Name' />
                 <label htmlFor="text">Email Address</label>
-                <input type="email" name="email" id="" placeholder='Enter Email' required />
+                <input type="email" name="email" id="2" placeholder='Enter Email' required />
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="" placeholder='Password' required />
+                <input type="password" name="password" id="3" placeholder='Password' required />
                 <input type="submit" value="Register" />
                 <p>Already have a account! <Link to={'/login'} className='login-btn' >Please Login</Link></p>
             </form>
