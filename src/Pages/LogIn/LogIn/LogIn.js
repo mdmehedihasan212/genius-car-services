a2726bd6195797d15fddab70fa0a953bc008f6a3
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-13 01:58:04 +0600

    add register form no framework

--- src/Pages/LogIn/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn/LogIn.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
 
@@ -34,16 +34,13 @@ const LogIn = () => {
                         <Form.Label>Password</Form.Label>
                         <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                     </Form.Group>
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
-                    <p className='text-center text-primary' onClick={handleRegistration}>Please Register</p>
+                    <p>New a genius car? <Link to={'/register'} className='text-center text-danger' onClick={handleRegistration}>Please Register</Link></p>
                 </Form>
             </div>
         </div>
--- src/Pages/LogIn/Register/Register.js
+++ src/Pages/LogIn/Register/Register.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Register</h1>
+            <form>
+                <label htmlFor="text">Name</label>
+                <input type="text" name="Your name" id="" placeholder='Your Name' />
+                <label htmlFor="text">Email Address</label>
+                <input type="email" name="email" id="" placeholder='Enter Email' required />
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input type="submit" value="Register" />
+                <p>Already have a account! <Link to={'/login'} className='login-btn' >Please Login</Link></p>
+            </form>
         </div>
     );
 };
