4d22f4bc49dc017eaf7b643d50f8e2d85afe516e
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-19 02:57:15 +0600

    add sentVarifiyed function into requireAuth

--- src/Pages/RequireAuth/RequireAuth.js
+++ src/Pages/RequireAuth/RequireAuth.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../Firebase.init';
 import Loading from '../Shared/Loading/Loading';
+import { ToastContainer, toast } from 'react-toastify';
 
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
 
     if (loading) {
         return <Loading></Loading>
     }
 
     if (!user) {
-        // Redirect them to the /login page, but save the current location they were
-        // trying to go to when they were redirected. This allows us to send them
-        // along to that page after they login, which is a nicer user experience
-        // than dropping them off on the home page.
+
         return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    if (!user.emailVerified) {
+        return <div className='mx-auto' style={{ width: "350px" }}>
+            <div className="card-body">
+                <h5 className="card-title">Your email not verified</h5>
+                <button className='btn btn-outline-primary' onClick={async () => {
+                    await sendEmailVerification();
+                    toast("Sent verify email")
+
+                }}
+                >Click verify your email</button>
+                <ToastContainer />
+            </div>
+        </div>
     }
     return children;
 };
